022809da0456a247ec476f146a7952c8d89ef1c5
liorwap <lliioorrn@gmail.com>
2021-02-02 11:08:36 +0200

    changed signup page to signup

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -5,7 +5,7 @@ import SignupComponenet from '../components/auth/SignupComponent'
 const Signup = () => {
     return (
         <Layout>
-            <h2 className="text-center pt-4 pd-4">Signup page</h2>
+            <h2 className="text-center pt-4 pd-4">Signup</h2>
             <div className="row">
                 <div className="col-md-6 offset-md-3">
                     <SignupComponenet />
