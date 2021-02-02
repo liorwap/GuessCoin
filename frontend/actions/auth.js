91a4cf4424f32f00f64c79bff6d1527909ea5b0c
liorwap <lliioorrn@gmail.com>
2021-02-02 12:11:01 +0200

    cookie and storage handling methods. set get and remove. needed to add another cookie-js package to handle that functionality

--- frontend/actions/auth.js
+++ frontend/actions/auth.js
@@ -1,5 +1,6 @@
 import fetch from 'isomorphic-fetch'
 import {API} from '../config'
+import cookie from 'js-cookie'
 
 export const signup = (user) => {
     return fetch(API + '/signup', {
@@ -13,3 +14,50 @@ export const signup = (user) => {
         .then(response => {return response.json();})
         .catch(error => console.log(error));
 };
+
+export const login = (user) => {
+    return fetch(API + '/login', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/ason',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+        .then(response => {return response.json();})
+        .catch(error => console.log(error));
+};
+
+export const setCookie = (key, value) => {
+    if(process.browser)
+    {
+        cookie.set(key, value, {
+            expires: '1d'
+        });
+    }
+};
+
+export const removeCookie = key => {
+    if(process.browser) {
+        cookie.remove(key);
+    }
+};
+
+export const getCookie = key => {
+    if(process.browser) {
+        cookie.get(key);
+    }
+}
+
+export const setLocalStorage = (key, value) => {
+    if(process.browser) {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+}
+
+export const removeLocalStorage = (key, value) => {
+    if(process.browser) {
+        localStorage.removeItem(key);
+    }
+}
+
